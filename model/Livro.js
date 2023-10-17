b96787bff6cb07283a78d9233da07110e6d18168
viictorst <victorsteixeira435@gmail.com>
2023-10-17 19:14:28 -0300

    Subindo novos arquivos

---
+++ model/Livro.js
@@ -0,0 +1,61 @@
+/*Importação do módulo do Sequelize*/
+const Sequelize = require('sequelize');
+
+/*Importação da conexão com o banco de dados*/
+const connection = require('../database/database');
+
+/*Importação da tabela de categoria para criação da chave estrangeira
+representanto a cardinalidade*/
+const Categoria = require('./Categoria');
+
+const Livro = connection.define(
+    'tbl_livro',
+    {
+        codigo_livro:{
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        codigo_categoria:{
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        titulo:{
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        preco:{
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        imagen_peq:{
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        imagen_grd:{
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        detalhes:{
+            type: Sequelize.TEXT,
+            allowNull: false
+        }
+    }
+);
+
+/*Implementação da  CHAVE ESTRANGEIRA - LADO N*/
+Categoria.hasMany(Livro, {
+    foreignKey: 'codigo_categoria',
+    sourceKey: 'codigo_categoria'
+});
+
+
+/*Implementação da  CHAVE PRIMÁRIA - LADO 1*/
+Livro.belongsTo(Categoria, {
+    foreignKey: 'codigo_categoria',
+    sourceKey: 'codigo_categoria'
+});
+
+Livro.sync({force:false});
+
+module.exports = Livro;
